d419b64855332f77ec761bba344207b030b76850
gogi999 <g.petrovic9@gmail.com>
2024-01-07 12:11:43 +0100

    Added CartItem, Checkout and Input components

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Input({ label, id, ...props }) {
+    return (
+        <p className="control">
+            <label htmlFor={id}>
+                {label}
+            </label>
+            <input id={id} name={id} required {...props} />
+        </p>
+    );
+}
+
+export default Input;
--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,11 +1,11 @@
-import React, {
+import {
   useEffect,
   useRef,
 } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ children, open, className = '' }) {
-   const dialog = useRef();
+export default function Modal({ children, open, onClose, className = '' }) {
+    const dialog = useRef();
 
     useEffect(() => {
         const modal = dialog.current;
@@ -16,12 +16,11 @@ function Modal({ children, open, className = '' }) {
 
         return () => modal.close();
     }, [open]);
-    
 
     return createPortal(
-        <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+        <dialog ref={dialog} className={`modal ${className}`} onClose={onClose}>
+            {children}
+        </dialog>,
         document.getElementById('modal')
     );
 }
-
-export default Modal;
